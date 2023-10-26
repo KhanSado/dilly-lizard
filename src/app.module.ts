2358968653fbf19c91c7c2286d3a9c10fa32b95d
bersoncrios <bersonintech@gmail.com>
2023-10-26 18:58:49 -0300

    ajustes de cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './authentication/auth.module';
 import { BookModule } from './book/book.module';
 import { AuthorModule } from './author/author.module';
 import { GenderModule } from './gender/gender.module';
+import * as cors from 'cors';
+
 @Module({
   imports: [UsersModule, AuthModule, BookModule, AuthorModule, GenderModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cors()).forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
